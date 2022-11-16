b318026c34e2d503a51d3aef0ba358cdb4bbda86
shafirassyf <118041829+shafirassyf@users.noreply.github.com>
2022-11-16 22:39:55 +0700

    Add files via upload

---
+++ js/UTShafira.js
@@ -0,0 +1,52 @@
+// ARTIKEL 1 READMORE
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+
+// ARTIKEL 2 READMORE
+function myFunctionn() {
+  var dots = document.getElementById("dotss");
+  var moreText = document.getElementById("moree");
+  var btnText = document.getElementById("myBtnn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+
+// ARTIKEL 3 READMORE
+function myFunctionnn() {
+  var dots = document.getElementById("dotsss");
+  var moreText = document.getElementById("moreee");
+  var btnText = document.getElementById("myBtnnn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
